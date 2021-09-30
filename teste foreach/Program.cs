688995ad636ff8718c4fe57786621519e2eae1b5
Hiury Gomes <hiury_jp@hotmail.com>
2021-09-30 13:41:38 -0300

    Teste foreach prof Nelio

---
+++ teste foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace teste_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] vet = new string[N];
+
+            for (int i=0; i<N; i++)
+            {
+                vet[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Nome lidos:");
+            foreach (string x in vet)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
